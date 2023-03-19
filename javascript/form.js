ddb8d3963b39d486e920bf95517d91a612c97fad
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-03-19 18:28:42 -0300

    continuo con la validacion dle formulario

---
+++ javascript/form.js
@@ -0,0 +1,37 @@
+(() => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+
+          fetch("https://formsubmit.co/ajax/costamariaeugenia1@gmail.com", {
+            method: "POST",
+            body: new FormData(event.target),
+          })
+            .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+            .then((json) => {
+              form.reset();
+            })
+            .catch((err) => {
+              console.log(err);
+            })
+            .finally(() => {
+              console.log("Enviado");
+            });
+
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
