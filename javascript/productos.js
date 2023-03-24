6bcab32cd4299a2e0b43db3a75e69720eea3c6e2
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-03-24 14:35:25 -0300

    Agrego el texto de quienes somos y cambio el +info por un pop over

---
+++ javascript/productos.js
@@ -0,0 +1,5 @@
+const d = document;
+const popoverTriggerList = d.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
